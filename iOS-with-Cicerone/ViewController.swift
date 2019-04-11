0523653eaa6976716d50d3c4bd27fb6117bcf4a4
Nikendo <pravdomir42@gmail.com>
2019-04-11 15:31:01 +0700

    Init commit

---
+++ iOS-with-Cicerone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-with-Cicerone
+//
+//  Created by Nikita Shmatov on 11/04/2019.
+//  Copyright © 2019 Intec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
